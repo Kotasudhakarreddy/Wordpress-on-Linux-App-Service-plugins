9005ee5f432211368fedf7c19bcb78f175f35700
billagopal <134916552+billagopal@users.noreply.github.com>
2023-06-26 17:38:41 +0530

    Updated code

--- azure-app-service-migration-plugin/admin/js/azure_app_service_migration-admin.js
+++ azure-app-service-migration-plugin/admin/js/azure_app_service_migration-admin.js
@@ -1,7 +1,18 @@
+$(document).ready(function(){
+	$("#confpassword").on('keyup',function(){
+		var password=$("#password").val();
+		var confpassword=$("#confpassword").val();
+		if(password!=confpassword){
+			$("#CheckPasswordMatch").html("Password does not match !").css("color","red");
+		}else{
+			$("#CheckPasswordMatch").html("Password match !").css("color","green");
+		}
+	});
+});
+
 jQuery(function(){
 	var ajaxurl=azure_app_service_migration.ajaxurl;
 	$("#blinkdata").hide();
-	
 	// hide funtion
 	function pageloadresonse(){
 		$("body").removeClass("loading"); 
@@ -10,15 +21,69 @@ jQuery(function(){
 		location.reload();
 	}
 
-	// Processing event on button click
-	jQuery(document).on("click", "#generatefile", function(){
+	// Ajax call before function
+	function pagebeforeloadresonse(){
 		$("#downloadfile").hide();
 		$("body").addClass("loading");
 		$("#blinkdata").show();
-		var postdata="action=admin_ajax_request&param=wp_filebackup";
+	}
+
+	// Processing event on button click
+	$("#generatefile").click(function(){
+		// password 
+		var password=$("#password").val();
+		$("#hiddenpassword").val(password);
+		// Confirm password
+		var confpassword=$("#confpassword").val();
+		$("#hiddenconfpassword").val(confpassword);
+		// Do not export post revisions 
+		if($("#dontexptpostrevisions").prop('checked') == true){
+			var dontexptpostrevisions=$("#dontexptpostrevisions").val();
+			$("#hiddendontexptpostrevisions").val(dontexptpostrevisions);
+		}
+		// Do not export media library (files) 
+		if($("#dontexptsmedialibrary").prop('checked') == true){
+			var dontexptsmedialibrary=$("#dontexptsmedialibrary").val();
+			$("#hiddendontexptsmedialibrary").val(dontexptsmedialibrary);
+		}
+		// Do not export themes (files) 
+		if($("#dontexptsthems").prop('checked') == true){
+			var dontexptsthems=$("#dontexptsthems").val();
+			$("#hiddendontexptsthems").val(dontexptsthems);
+		}
+		// Do not export must-use plugins (files) 
+		if($("#dontexptmustuseplugs").prop('checked') == true){
+			var dontexptmustuseplugs=$("#dontexptmustuseplugs").val();
+			$("#hiddendontexptmustuseplugs").val(dontexptmustuseplugs);
+		}
+		// Do not export plugins (files) 
+		if($("#dontexptplugins").prop('checked') == true){
+			var dontexptplugins=$("#dontexptplugins").val();
+			$("#hiddendontexptplugins").val(dontexptplugins);
+		}
+		// Do not export database (sql)
+		if($("#dbsql").prop('checked') == true){
+			var dbsql=$("#dbsql").val();
+			$("#hiddendbsql").val(dbsql);
+		}
+		// password and confirm password validations
+		function pwdvalid(){
+			if($("#password").val()==''){
+				alert('Please Enter Password');
+			}
+			if($("#confpassword").val()==''){
+				alert('Please Enter Password');
+			}
+		}
+		// Form submition
+		// pwdvalid();
+		pagebeforeloadresonse();
+		var postdata=$("#frm-chkbox-data").serialize();
+		postdata += "&action=admin_ajax_request&param=wp_filebackup";
 		jQuery.post(ajaxurl,postdata, function(response){
 			var data=jQuery.parseJSON(response);
-			//console.log(response);
+			console.log(response);
+			//console.log(postdata);
 			if(data.status==1){
 				alert(data.message);
 				pageloadresonse();
@@ -27,210 +92,4 @@ jQuery(function(){
 			}
 		});
 	});
-
-	// Do not Export database(sql)
-	$('#dbsql').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading");
-			var postdata="action=admin_ajax_request&param=wp_donotexportsql";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					alert(data.message);
-					$("body").removeClass("loading"); 
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-		   // do what you need here         
-		   //alert("Unchecked");
-		}
-	});
-
-	// Protect this backup with a password
-	$('#protectbkuppwd').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading");
-			var postdata="action=admin_ajax_request&param=wp_protectbkuppwd";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
-
-	// Do not export media library (files)
-	$('#dontexptsmedialibrary').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading");
-			var postdata="action=admin_ajax_request&param=wp_dontexptsthems";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
-
-	// Do not export media library (files)
-	$('#dontexptsthems').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading"); 
-			var postdata="action=admin_ajax_request&param=wp_dontexptsthems";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
-
-	// Do not export spam comments
-	$('#dontexptspamcmt').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading"); 
-			var postdata="action=admin_ajax_request&param=wp_dontexptspamcmt";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					setTimeout(function(){
-						location.reload();
-					}, 1000);
-					//$("#downloadfile").load();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
-
-	// Do not export plugins
-	$('#dontexptplugins').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading"); 
-			var postdata="action=admin_ajax_request&param=wp_dontexptplugins";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					//$("#downloadfile").load();
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			// alert("Unchecked");
-		}
-	});
-
-	// Do not export post revisions
-	$('#dontexptpostrevisions').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading"); 
-			var postdata="action=admin_ajax_request&param=wp_dontexptpostrevisions";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					//$("#downloadfile").load();
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
-
-	// Do not export post revisions
-	$('#dontreplaceemaildomain').click(function () {
-		if ($(this).prop('checked')) {
-			$("#downloadfile").hide();
-			$("body").addClass("loading"); 
-			var postdata="action=admin_ajax_request&param=wp_dontreplaceemaildomain";
-			jQuery.post(ajaxurl,postdata, function(response){
-				var data=jQuery.parseJSON(response);
-				//console.log(response);
-				if(data.status==1){
-					$("body").removeClass("loading"); 
-					alert(data.message);
-					//$("#downloadfile").load();
-					location.reload();
-					$("#downloadfile").show();
-				}else{
-					alert(data.message);
-				}
-			});
-		}else {
-			// do what you need here         
-			//alert("Unchecked");
-		}
-	});
 });
-// $(document).on({
-// 	ajaxStart: function(){
-// 		$("body").addClass("loading"); 
-// 	},
-// 	ajaxStop: function(){ 
-// 		$("body").removeClass("loading"); 
-// 	}    
-// });
